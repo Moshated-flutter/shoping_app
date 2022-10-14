31b140b288871d50464457c49ddff4718911baa6
moshated <mobin.cofingfluter@gmail.com>
2022-10-14 21:20:45 +0330

    working on delete cart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: unnecessary_null_comparison
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CartItem {
   final String id;
@@ -33,21 +36,48 @@ class Cart with ChangeNotifier {
     return total;
   }
 
-  void addCart(String productId, double price, String title) {
+  void addCart(String productId, double price, String title) async {
+    const urlnew =
+        'https://shopapp-a5aa1-default-rtdb.firebaseio.com/cart.json';
     if (_items.containsKey(productId)) {
+      var findedaddress = _items[productId]!.id;
+      final urlUpdate =
+          'https://shopapp-a5aa1-default-rtdb.firebaseio.com/cart/$findedaddress.json';
       _items.update(
-          productId,
-          (value) => CartItem(
-                id: value.id,
-                title: value.title,
-                amount: value.amount + 1,
-                price: value.price,
-              ));
+        productId,
+        (value) {
+          http.patch(
+            Uri.parse(
+              urlUpdate,
+            ),
+            body: json.encode({
+              'title': value.title,
+              'price': value.price,
+              'amount': value.amount + 1,
+            }),
+          );
+          return CartItem(
+            id: value.id,
+            title: value.title,
+            amount: value.amount + 1,
+            price: value.price,
+          );
+        },
+      );
     } else {
+      final responseCart = await http.post(
+        Uri.parse(urlnew),
+        body: json.encode({
+          'idProduct': productId,
+          'title': title,
+          'price': price,
+          'amount': 1,
+        }),
+      );
       _items.putIfAbsent(
           productId,
           () => CartItem(
-                id: DateTime.now().toString(),
+                id: json.decode(responseCart.body)['name'],
                 title: title,
                 amount: 1,
                 price: price,
@@ -56,7 +86,7 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
-  void removeitem(String productid) {
+  void removeitem(String productid) async {
     _items.remove(productid);
     notifyListeners();
   }
@@ -66,7 +96,7 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
-  void removesingleItem(String singleItemid) {
+  void removesingleItem(String singleItemid) async {
     if (!_items.containsKey(singleItemid)) {
       return;
     }
@@ -82,6 +112,27 @@ class Cart with ChangeNotifier {
     } else {
       _items.remove(singleItemid);
     }
+    notifyListeners();
+  }
+
+  Future<void> fetchAndSetCart() async {
+    const url = 'https://shopapp-a5aa1-default-rtdb.firebaseio.com/cart.json';
+
+    final response = await http.get(Uri.parse(url));
+    print(json.decode(response.body));
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    final Map<String, CartItem> loadedCart = {};
+    extractedData.forEach((key, value) {
+      loadedCart.addAll({
+        key: CartItem(
+          amount: value['amount'],
+          price: value['price'],
+          id: value['idProduct'],
+          title: value['title'],
+        )
+      });
+    });
+    _items = loadedCart;
     notifyListeners();
   }
 }
